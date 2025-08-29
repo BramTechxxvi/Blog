719aa198ff6f8aaa62f23969324c5351b76e7943
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-29 14:58:05 +0100

    update

--- article/serializers.py
+++ article/serializers.py
@@ -1,19 +1,13 @@
 from rest_framework import serializers
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
-
+from .models import User
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
+        model = User
         fields = ['username', 'email', 'password', 'phone']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
-
-
-        class UserCreateSerializer(BaseUserCreateSerializer):
-            class Meta(BaseUserCreateSerializer.Meta):
-                fields = ['username', 'first_name', 'last_name', 'email', 'password', 'phone']
-
-        class CreateInviteSerializer(serializers.Serializer):
-            first_name = serializers.CharField(max_length=255, allow_blank=False)
-            last_name = serializers.CharField(max_length=255, allow_blank=False)
-            phone = serializers.CharField(max_length=11, min_length=11, allow_blank=False)
-            expires_at = serializers.DateTimeField()
+class PostSerializer(serializers.ModelSerializer):
