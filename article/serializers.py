4dbc2a931becb5929ffdc6c11cd904eed7551821
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-29 16:44:36 +0100

    update

--- article/serializers.py
+++ article/serializers.py
@@ -18,3 +18,77 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['title', 'content', 'created_at', 'user']
 
+        # from rest_framework.decorators import api_view, permission_classes
+        # from rest_framework.permissions import IsAuthenticated
+        # from rest_framework.response import Response
+        # from rest_framework import status
+        # from django.shortcuts import get_object_or_404
+        # from .models import Post, Comment, Like, Repost
+        # from .serializers import PostSerializer, CommentSerializer, LikeSerializer, RepostSerializer
+        # from rest_framework.response import Response
+        # from rest_framework import status
+        # from .serializers import UserCreateSerializer
+
+        @api_view(['POST'])
+        @permission_classes([AllowAny])  # Anyone can register
+        def register_user(request):
+            serializer = UserCreateSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+                return Response({
+                    "message": "User registered successfully",
+                    "user": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email,
+                        "phone": user.phone,
+                    }
+                }, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # --- POSTS ---
+        # @api_view(['GET', 'POST'])
+        # @permission_classes([IsAuthenticated])
+        # def posts(request):
+        #     if request.method == 'GET':
+        #         posts = Post.objects.all().order_by('-created_at')
+        #         serializer = PostSerializer(posts, many=True)
+        #         return Response(serializer.data)
+        #
+        #     if request.method == 'POST':
+        #         serializer = PostSerializer(data=request.data)
+        #         serializer.is_valid(raise_exception=True)
+        #         serializer.save(user=request.user)
+        #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # --- COMMENTS ---
+        # @api_view(['POST'])
+        # @permission_classes([IsAuthenticated])
+        # def add_comment(request, post_id):
+        #     post = get_object_or_404(Post, id=post_id)
+        #     serializer = CommentSerializer(data=request.data)
+        #     serializer.is_valid(raise_exception=True)
+        #     serializer.save(user=request.user, post=post)
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # --- LIKES ---
+        # @api_view(['POST'])
+        # @permission_classes([IsAuthenticated])
+        # def add_like(request, post_id):
+        #     post = get_object_or_404(Post, id=post_id)
+        #     serializer = LikeSerializer(data=request.data)
+        #     serializer.is_valid(raise_exception=True)
+        #     serializer.save(user=request.user, post=post)
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # --- REPOSTS ---
+        # @api_view(['POST'])
+        # @permission_classes([IsAuthenticated])
+        # def repost(request, post_id):
+        #     post = get_object_or_404(Post, id=post_id)
+        #     serializer = RepostSerializer(data={'post': post.id})
+        #     serializer.is_valid(raise_exception=True)
+        #     serializer.save(user=request.user, post=post)
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
--- article/views.py
+++ article/views.py
@@ -1,10 +1,48 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
+from requests import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_fra
+
+from article.serializers import UserCreateSerializer
 
 
 # Create your views here.
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def register_user(request):
+    serializer = UserCreateSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        return Response({
+            "message": "Registered succesfully",
+        "user" : {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+        }
+        }
+        )
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+
+    # class CommentSerializer(serializers.ModelSerializer):
+    #     user = serializers.StringRelatedField(read_only=True)
+    #
+    #     class Meta:
+    #         model = Comment
+    #         fields = ['id', 'content', 'created_at', 'user', 'post']
+    #
+    # # Like Serializer
+    # class LikeSerializer(serializers.ModelSerializer):
+    #     class Meta:
+    #         model = Like
+    #         fields = ['id', 'type', 'user', 'post']
+    #
+    # # Repost Serializer
+    # class RepostSerializer(serializers.ModelSerializer):
+    #     class Meta:
+    #         model = Repost
+    #         fields = ['id', 'user', 'post', 'created_at']
