bda659def1e2c822603a1398eb8313e4f1b1887a
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-29 12:41:28 +0100

    update

--- Blog/settings.py
+++ Blog/settings.py
@@ -131,3 +131,16 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'article.User'
+
+# DJOSER = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     )
+# }
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'article.serializers.UserCreateSerializer',
+    }
+}
+
---
+++ article/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['username', 'email', 'password', 'phone']
