03667e2f17ad8ac7ec098c7e9a6dce75d2d00f3b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-29 11:19:34 +0100

    update

--- article/models.py
+++ article/models.py
@@ -9,11 +9,13 @@ class User(AbstractUser):
     password = models.CharField(null=False, max_length=16)
     phone = models.CharField(null=False, unique=True, max_length=11)
 
+
 class Post(models.Model):
     title = models.CharField(null=False)
     content = models.TextField(null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 class Comment(models.Model):
     content = models.TextField(null=False)
@@ -21,10 +23,21 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
+
 class Like(models.Model):
-    type
-
-
-ost = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
-    content = models.TextField(null=False)
+    LIKE_CHOICES = [
+        ('like', 'like'),
+        ('love', 'love'),
+        ('angry', 'Angry'),
+        ('sad', 'Sad')
+    ]
+    type = models.CharField(choices=LIKE_CHOICES, default='like')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Repost(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reposts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='reposts')
