28e58fd7d4b2c98f7324028ad36e2e7b72a69541
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-29 12:00:35 +0100

    update

--- article/admin.py
+++ article/admin.py
@@ -1,21 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User, Post
-#
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ("id", "content", "user", "post", "created_at")
-#     search_fields = ("content",)
-#     list_filter = ("created_at",)
-#
-# @admin.register(Like)
-# class LikeAdmin(admin.ModelAdmin):
-#     list_display = ("id", "type", "user", "post")
-#
-# @admin.register(Repost)
-# class RepostAdmin(admin.ModelAdmin):
-#     list_display = ("id", "user", "post", "created_at")
-
+from .models import User, Post, Comment, Like, Repost
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
@@ -28,15 +13,17 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'created_at', 'updated_at']
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'user', 'post', 'created_at']
 
 
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'type', 'user', 'post', 'created_at']
 
 
-# Register your models here.
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ("id", "title", )
-#     search_fields = ("title", "content")
-#     list_filter = ("created_at",)
-
+@admin.register(Repost)
+class RepostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'post', 'created_at']
 
